2e9c9d28c6a3e10ffc73302ab2814b039c6fc27e
sayem-hasnat <mdsayemhasnat@gmail.com>
2021-01-21 15:57:29 +0600

    Data Initialize Code added

---
+++ src/main/java/com/example/studentmanagement/init/DataInitializer.java
@@ -0,0 +1,17 @@
+package com.example.studentmanagement.init;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        /*---------- For Checking Purpose ---------*/
+        System.out.println("Data Initialize Execute");
+
+    }
+}
